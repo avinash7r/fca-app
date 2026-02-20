693fb3b7f2cbaec0f227058022ac51b718cedbc3
Avinash Rajure <rajureavinash7@gmail.com>
2026-02-20 10:37:32 +0530

    changed nginx-config

--- client/src/lib/axiosInstance.js
+++ client/src/lib/axiosInstance.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-    baseURL: import.meta.env.MODE === "development" ? "http://localhost:7777/api" : "/api",
-    withCredentials: true,
+  baseURL: "/api",
+  withCredentials: true,
 });
