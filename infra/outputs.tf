f60d311842b3aac432db0101b6a18cc793162003
Avinash Rajure <rajureavinash7@gmail.com>
2026-06-26 20:57:11 +0530

    test: deploy step

--- infra/outputs.tf
+++ infra/outputs.tf
@@ -10,10 +10,18 @@ output "github_actions_role_arn" {
   value = aws_iam_role.github_actions.arn
 }
 
+output "server_private_ip" {
+  value = aws_instance.server.private_ip
+}
+
 output "ecr_client_url" {
   value = aws_ecr_repository.client.repository_url
 }
 
 output "ecr_server_url" {
   value = aws_ecr_repository.server.repository_url
+}
+
+output "alb_dns" {
+  value = aws_lb.main.dns_name
 }
